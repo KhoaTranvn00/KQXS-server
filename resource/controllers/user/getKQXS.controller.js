cb3ea96a44df2e856f2c42991bb0d8f00dccbf34
05012000aA <anhkhoacttv@gamil.com>
2022-03-10 14:00:56 +0700

    get kqxs thu dc 1 ngay

--- resource/controllers/user/getKQXS.controller.js
+++ resource/controllers/user/getKQXS.controller.js
@@ -54,6 +54,85 @@ const getKQXS = {
 			});
 		}
 	},
+
+	thuMN: async (req, res) => {
+		const { slug } = req.params;
+		let ketquas = [];
+		try {
+			const thu = await thuModel.findOne({ slug: slug }).populate("dai.mn");
+			const dais = thu.dai.mn;
+
+			let ngay = new Date();
+			while (ngay.getDay() !== thu._id) {
+				ngay.setDate(ngay.getDate() - 1);
+			}
+
+			for (const dai of dais) {
+				try {
+					let preNgay = new Date();
+					preNgay.setDate(ngay.getDate() - 1);
+					const query = {
+						dai: dai._id,
+						ngay: {
+							$gte: preNgay,
+							$lt: ngay,
+						},
+					};
+					console.log(query);
+					const ketquadai = await ketquaModel.findOne(query).populate("dai");
+					if (ketquadai === null)
+						return res.status(400).json({
+							success: false,
+							message: `Đài ${dai.ten} chưa có kết quả`,
+						});
+					ketquas.push(ketquadai);
+				} catch (error) {
+					res.status(400).json({
+						success: false,
+						message: "Lấy KQ thất bại",
+						error: error,
+					});
+				}
+			}
+			res.status(200).json({
+				success: true,
+				message: "Lấy KQXS thành công",
+				ngay,
+				thu: thu.thu,
+				ketquas,
+			});
+		} catch (error) {
+			res.status(400).json({
+				success: false,
+				message: "Thứ nhập vào không hợp lệ",
+				error: error,
+			});
+		}
+		res.send("ok");
+	},
+};
+
+const thu = async (dais, ngay) => {
+	let ketquas = [];
+	for (const dai of dais) {
+		try {
+			let preNgay = new Date();
+			preNgay.setDate(ngay.getDate() - 1);
+			const query = {
+				dai: dai._id,
+				ngay: {
+					$gte: preNgay,
+					$lt: ngay,
+				},
+			};
+			const ketquadai = await ketquaModel.findOne(query).populate("dai");
+			if (ketquadai === null) return ketquas;
+			ketquas.push(ketquadai);
+		} catch (error) {
+			return ketquas;
+		}
+	}
+	return ketquas;
 };
 
 module.exports = getKQXS;
--- resource/routes/user/getKQXS.route.js
+++ resource/routes/user/getKQXS.route.js
@@ -3,5 +3,6 @@ const route = require("express").Router();
 const controller = require("../../controllers/user/getKQXS.controller");
 
 route.get("/mien-nam/ngay/:ngay", controller.ngayMN);
+route.get("/mien-nam/thu/:slug", controller.thuMN);
 
 module.exports = route;
