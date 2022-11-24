068c7fa4b75183a8b90f0faae0a2ebbbe41cba76
Khoa Tran Anh <takhoa1@tma.com.vn>
2022-11-24 17:19:37 +0700

    24c

--- resource/routes/utils/pay.route.js
+++ resource/routes/utils/pay.route.js
@@ -69,6 +69,35 @@ route.post("/create_payment_url", function (req, res, next) {
 	return res.json({ url: vnpUrl });
 });
 
+route.get("/vnpay_return", function (req, res, next) {
+	var vnp_Params = req.query;
+
+	var secureHash = vnp_Params["vnp_SecureHash"];
+
+	delete vnp_Params["vnp_SecureHash"];
+	delete vnp_Params["vnp_SecureHashType"];
+
+	vnp_Params = sortObject(vnp_Params);
+
+	var config = require("config");
+	var tmnCode = config.get("vnp_TmnCode");
+	var secretKey = config.get("vnp_HashSecret");
+
+	var querystring = require("qs");
+	var signData = querystring.stringify(vnp_Params, { encode: false });
+	var crypto = require("crypto");
+	var hmac = crypto.createHmac("sha512", secretKey);
+	var signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+
+	if (secureHash === signed) {
+		//Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
+		return res.json({ success: true });
+		// res.render('success', {code: vnp_Params['vnp_ResponseCode']})
+	} else {
+		return res.json({ success: false });
+	}
+});
+
 function sortObject(obj) {
 	var sorted = {};
 	var str = [];
